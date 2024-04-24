4b9153fe571130b4de29affcbe9ceb24dd27610f
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-24 22:05:32 +0200

    feat: domain entity

---
+++ src/Services/Catalog.Api/Domain/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.Api.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+        public List<string>? Categories { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(Guid id, string name, string? description, List<string>? categories, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Categories = categories;
+            Price = price;
+        }
+    }
+}
