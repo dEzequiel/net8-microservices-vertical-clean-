f4095f1a12a9fe9c16b587b3363ad4378e609104
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-24 22:06:09 +0200

    feat: building blocks cqrs handler definition

---
+++ src/Crosscutting/Crosscutting.CQRS/Infrastructure/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Crosscutting.CQRS.Domain;
+using MediatR;
+
+namespace Crosscutting.CQRS.Infrastructure
+{
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    { }
+
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    { }
+}
---
+++ src/Crosscutting/Crosscutting.CQRS/Infrastructure/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using Crosscutting.CQRS.Domain;
+using MediatR;
+
+namespace Crosscutting.CQRS.Infrastructure
+{
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
