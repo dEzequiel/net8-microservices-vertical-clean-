029227259821b58db27c6244f6fd47b71aef7341
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-24 22:05:52 +0200

    feat: building blocks cqrs domain messages definition

---
+++ src/Crosscutting/Crosscutting.CQRS/Domain/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crosscutting.CQRS.Domain
+{
+    public interface ICommand : IRequest<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse> { }
+}
---
+++ src/Crosscutting/Crosscutting.CQRS/Domain/IQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crosscutting.CQRS.Domain
+{
+    public interface IQuery<out TResponse> : IRequest<TResponse>
+        where TResponse : notnull;
+    
+    
+}
