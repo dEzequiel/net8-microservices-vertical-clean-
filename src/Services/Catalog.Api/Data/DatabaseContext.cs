ca6b0ff712d8bc2d205077742847dc2359a87dbe
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-26 00:26:29 +0200

    feat: db context

---
+++ src/Services/Catalog.Api/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Catalog.Api.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
