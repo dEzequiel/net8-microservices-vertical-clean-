556e423641e23515b1dbb7524d29c12bfd1007f5
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-26 00:27:03 +0200

    feat: add product feature implementation

---
+++ src/Services/Catalog.Api/Features/FindProductById/FindProductByIdEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Carter;
+using Catalog.Api.Domain;
+using Catalog.Api.Message.Query;
+using MediatR;
+
+namespace Catalog.Api.Features.FindProductById
+{
+    public record FindProductByIdResponse(Product Product);
+    public class FindProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender _mediator) =>
+            {
+                var query = new FindProductByIdQuery(id);
+                var result = await _mediator.Send(query);
+                var response = new FindProductByIdResponse(result.Product);
+                return Results.Ok(response);
+            });
+        }
+    }
+}
---
+++ src/Services/Catalog.Api/Features/FindProductById/FindProductByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Message.Query;
+using Crosscutting.CQRS.Infrastructure;
+
+namespace Catalog.Api.Features.FindProductById
+{
+    public class FindProductByIdQueryHandler : IQueryHandler<FindProductByIdQuery, FindProductByIdQueryResponse>
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public FindProductByIdQueryHandler(DatabaseContext databaseContext) =>
+            _databaseContext = databaseContext;
+        
+
+        public async Task<FindProductByIdQueryResponse> Handle(FindProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var id = query.Id;
+            var product = await _databaseContext.Products.FindAsync(id);
+
+            if (product is null)
+                throw new NullReferenceException();
+
+            var response = new FindProductByIdQueryResponse(product);
+            return response;
+        }
+    }
+}
