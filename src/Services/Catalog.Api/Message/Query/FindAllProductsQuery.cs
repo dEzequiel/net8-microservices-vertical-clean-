3f2e9c6f0247d2d9220d7a4a745b9a25d67972db
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-25 23:02:03 +0200

    feat: query messages implpementation

---
+++ src/Services/Catalog.Api/Message/Query/FindAllProductsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Api.Domain;
+using Crosscutting.CQRS.Domain;
+
+namespace Catalog.Api.Message.Query
+{
+    public class FindAllProductsQuery : IQuery<FindAllProductsQueryResponse>
+    {
+    }
+
+    public record FindAllProductsQueryResponse(List<Product> Products);
+}
---
+++ src/Services/Catalog.Api/Message/Query/FindProductByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Api.Domain;
+using Crosscutting.CQRS.Domain;
+
+namespace Catalog.Api.Message.Query
+{
+    public class FindProductByIdQuery : IQuery<FindProductByIdQueryResponse>
+    {
+        public Guid Id { get; private set; }
+        public FindProductByIdQuery(Guid id) => Id = id;
+    }
+
+    public record FindProductByIdQueryResponse(Product Product);
+}
