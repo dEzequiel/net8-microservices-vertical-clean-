8b345850320abaf3e6519a3a3a342550c0bb92ef
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-26 00:26:56 +0200

    feat: add product feature implementation

---
+++ src/Services/Catalog.Api/Features/AddProduct/AddProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Domain;
+using Catalog.Api.Message.Command;
+using Crosscutting.CQRS.Infrastructure;
+
+namespace Catalog.Api.Features.AddProduct
+{
+    public class AddProductCommandHandler : ICommandHandler<AddProductCommand, AddProductCommandResponse>
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public AddProductCommandHandler(DatabaseContext dbContext) =>
+            _dbContext = dbContext;
+        
+
+        public async Task<AddProductCommandResponse> Handle(AddProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product(Guid.NewGuid(), command.Name, command.Description, command.Categories, command.Price);
+            
+            await _dbContext.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+
+            var response = new AddProductCommandResponse(product.Id);
+            return response;
+        }
+    }
+}
---
+++ src/Services/Catalog.Api/Features/AddProduct/AddProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using Catalog.Api.Domain;
+using Catalog.Api.Message.Command;
+using MediatR;
+
+namespace Catalog.Api.Features.AddProduct
+{
+    public record AddProductRequest(string name, string? description, List<string>? categories, decimal price);
+    public record AddProductResponse(Guid Id);
+    public class AddProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (AddProductRequest request, ISender _mediator) =>
+            {
+                var command = new AddProductCommand(request.name, request.description, request.categories, request.price);
+                var result = await _mediator.Send(command);
+                var response = new AddProductResponse(result.Id);
+                return Results.CreatedAtRoute($"/products/{response.Id}", response);
+            });
+        }
+    }
+}
