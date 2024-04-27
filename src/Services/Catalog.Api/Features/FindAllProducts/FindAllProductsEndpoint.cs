0b28c0cb2faad0b1c8aa2d3c8a861ddee8c0eb13
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-27 20:59:55 +0200

    feat: find all products endpoint and handler

---
+++ src/Services/Catalog.Api/Features/FindAllProducts/FindAllProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Carter;
+using Catalog.Api.Domain;
+using Catalog.Api.Message.Query;
+using MediatR;
+
+namespace Catalog.Api.Features.FindAllProducts
+{
+    public record FindAllProductsResponse(List<Product> Products);
+    public class FindAllProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender _mediator) =>
+            {
+                var query = new FindAllProductsQuery();
+                var result = await _mediator.Send(query);
+                var response = new FindAllProductsResponse(result.Products);
+                return Results.Ok(response);
+            });
+        }
+    }
+}
---
+++ src/Services/Catalog.Api/Features/FindAllProducts/FindAllProductsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Message.Query;
+using Crosscutting.CQRS.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.Features.FindAllProducts
+{
+    public class FindAllProductsQueryHandler : IQueryHandler<FindAllProductsQuery, FindAllProductsQueryResponse>
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public FindAllProductsQueryHandler(DatabaseContext databaseContext) =>
+            _databaseContext = databaseContext;
+
+
+        public async Task<FindAllProductsQueryResponse> Handle(FindAllProductsQuery query, CancellationToken cancellationToken)
+        {
+            var products = await _databaseContext.Products.AsNoTracking().ToListAsync(cancellationToken);    
+            var response = new FindAllProductsQueryResponse(products);
+            return response;
+        }
+    }
+}
