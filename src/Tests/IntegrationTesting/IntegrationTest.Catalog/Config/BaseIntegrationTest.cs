cee251c9ac2cb5b582fbec87aeed9dc8664535aa
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-30 00:42:14 +0200

    test: integration test configuration with testcontainers

---
+++ src/Tests/IntegrationTesting/IntegrationTest.Catalog/Config/BaseIntegrationTest.cs
@@ -0,0 +1,18 @@
+﻿namespace IntegrationTest.Catalog.Config
+{
+    public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestApplicationFactory>
+    {
+        private readonly IServiceScope _scope;
+        protected readonly ISender _sender;
+        protected readonly DatabaseContext _dbContext;
+        protected HttpClient _client;
+
+        protected BaseIntegrationTest(IntegrationTestApplicationFactory factory)
+        {
+            _scope = factory.Services.CreateScope();
+            _sender = _scope.ServiceProvider.GetRequiredService<ISender>();
+            _dbContext = _scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+            _client = factory.CreateClient();
+        }
+    }
+}
---
+++ src/Tests/IntegrationTesting/IntegrationTest.Catalog/Config/IntegrationTestApplicationFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Testcontainers.PostgreSql;
+
+namespace IntegrationTest.Catalog.Config
+{
+    public class IntegrationTestApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:latest")
+            .WithDatabase("integration_tests")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(srv =>
+            {
+                var descriptor = srv.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<DatabaseContext>));
+
+                if (descriptor is not null)
+                    srv.Remove(descriptor);
+
+                srv.AddDbContext<DatabaseContext>(opt =>
+                {
+                    //var postgresConnectionString = $"Host=localhost;Port=5433;Database=integration_tests_{dbSuffix};Username=postgres;Password=postgres;Maximum Pool Size=1";
+                    opt.UseNpgsql(_dbContainer.GetConnectionString());
+                });
+            });
+        }
+
+        public Task InitializeAsync()
+        {
+            return _dbContainer.StartAsync();
+        }
+
+        public Task DisposeAsync()
+        {
+            return _dbContainer.StopAsync();
+        }
+    }
+}
