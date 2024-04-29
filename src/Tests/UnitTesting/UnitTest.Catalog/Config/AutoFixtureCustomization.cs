4e7d63cd534813674ef824d86ceb1518b50d0547
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-04-29 02:50:49 +0200

    chore: configuration for testing with moq and autofixture

---
+++ src/Tests/UnitTesting/UnitTest.Catalog/Config/AutoFixtureCustomization.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+
+namespace UnitTest.Catalog.Config
+{
+    public class AutoFixtureCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize(new AutoMoqCustomization());
+
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        }
+    }
+}
---
+++ src/Tests/UnitTesting/UnitTest.Catalog/Config/AutoMoqDataAttribute.cs
@@ -0,0 +1,12 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+
+
+namespace UnitTest.Catalog.Config
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            : base(() => new Fixture().Customize(new AutoFixtureCustomization())) { }
+    }
+}
